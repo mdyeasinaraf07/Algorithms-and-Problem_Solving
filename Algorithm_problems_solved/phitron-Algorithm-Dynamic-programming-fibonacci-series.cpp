32a737496b75ebb1f196924cf958b30753e3160e
Md Yeasin Arafat <116306992+mdyeasinaraf07@users.noreply.github.com>
2023-05-05 01:40:50 +0600

    Update phitron-Algorithm-Dynamic-programming-fibonacci-series.cpp

--- Algorithm_problems_solved/phitron-Algorithm-Dynamic-programming-fibonacci-series.cpp
+++ Algorithm_problems_solved/phitron-Algorithm-Dynamic-programming-fibonacci-series.cpp
@@ -1,17 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long int arr[102];
+long long int dp[102];
 
 long long int fib(int n){
     if(n <= 2){
         return 1;
     }
-    if(arr[n] > 0){
-        return arr[n];
+    if(dp[n] != -1){
+        return dp[n];
     }
-    arr[n] = fib(n - 1) + fib(n - 2);
-    return arr[n];
+    long long int ans = fib(n - 1) + fib(n - 2);
+    dp[n] = ans;
+    return ans;
 }
 
 // fib series: 1, 1, 2, 3, 5, 8, 13, 21, 34, .......
@@ -19,6 +20,9 @@ long long int fib(int n){
 int main(){
     int n;
     cin>> n;
+    for(int i = 1; i <= n; i++){
+        dp[i] = -1;
+    }
     long long int ans = fib(n);
     cout<< ans<< endl;
 
